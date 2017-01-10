032896bee7634a0b3b82067eb4f8125f64a59651
MelissaLoggins <melissaloggins@gmail.com>
2017-01-10 15:10:40 -0500

    initial html layout complete 2

---
+++ javascript/logic.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDBCt45kn53tBpA4PA32ZuXkcVDXrNvlaM",
+    authDomain: "train-scheduler-f6ff8.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f6ff8.firebaseio.com",
+    storageBucket: "train-scheduler-f6ff8.appspot.com",
+    messagingSenderId: "98588375073"
+  };
+  firebase.initializeApp(config);
